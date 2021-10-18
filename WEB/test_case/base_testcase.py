00d5cc1e339fd3be1ebcd4108c405d5e373b2d9c
zc <zhangchanget@126.com>
2021-10-18 11:49:43 +0800

    2021.10.18更新基础用例模板

--- WEB/test_case/base_testcase.py
+++ WEB/test_case/base_testcase.py
@@ -3,3 +3,20 @@
 # @Time    : 2021/08/26 10:06
 # @Author  : zc
 # @File    : base_testcase.py
+
+from WEB.page.web import Web
+from WEB.utils.functions import Functions as Fun
+
+
+class BaseTestCase:
+    loginData = Fun().getYamlData("login")
+
+    def setup(self):
+        """开启测试用例"""
+        self.web = Web().startWeb(self.loginData['url'])
+        self.login = self.web.goto_loginPage()
+
+    def teardown(self):
+        """关闭测试用例"""
+        # Web().stopWeb()
+        self.web.stopWeb()
